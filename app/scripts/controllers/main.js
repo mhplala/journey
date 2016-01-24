33906f4603b047baab525494cc747cedbb8e8741
王金鑫 <mhplala@gmail.com>
2016-01-25 03:12:21 +0800

    init

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name journeyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the journeyApp
+ */
+angular.module('journeyApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
